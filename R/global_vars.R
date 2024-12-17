3d91be51e2764e236a427f8eede7e476853dffb8
Brian Helsel <bhelsel@kumc.edu>
2024-12-17 10:30:50 -0600

    Update imports and global variables

--- R/global_vars.R
+++ R/global_vars.R
@@ -1,3 +1,5 @@
 if(getRversion() >= "2.15.1") utils::globalVariables(c(
-  "codebook", "data_dictionary", "subject_label", "event_sequence"
+  ".", "codebook", "data_dictionary", "subject_label", "event_sequence",
+  "verbal", "nonverbal", "min_age", "max_age", "raw_score", "iq", "ae",
+  "sum_of_standard_scores", "verbal_raw_score", "nonverbal_raw_score"
 ))
